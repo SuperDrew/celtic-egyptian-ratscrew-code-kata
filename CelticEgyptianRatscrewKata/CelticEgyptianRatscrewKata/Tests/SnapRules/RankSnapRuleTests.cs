3c3d00c7d15a7c5435f2f5b4cc7a47ae29d95839
SuperDrew <andrew.fraser@red-gate.com>
2015-03-20 11:57:33 +0000

    Added RankSnapRule and failing test.

---
+++ CelticEgyptianRatscrewKata/CelticEgyptianRatscrewKata/Tests/SnapRules/RankSnapRuleTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CelticEgyptianRatscrewKata.Game;
+using CelticEgyptianRatscrewKata.SnapRules;
+using NUnit.Framework;
+
+namespace CelticEgyptianRatscrewKata.Tests.SnapRules
+{
+    [TestFixture]
+    public class RankSnapRuleTests
+    {
+        private RankSnapRule _rankSnapRule;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _rankSnapRule = new RankSnapRule();
+        }
+
+        [Test]
+        public void SnapFailWithEmptyCardStack()
+        {
+            //Arrange
+            IReadOnlyGameState gameState = new ReadOnlyGameState {Stack = Cards.Empty()};
+
+            //Act
+            var result = _rankSnapRule.IsSnapValid(gameState);
+
+            //Assert
+            Assert.IsFalse(result);
+        }
+
+
+    }
+}
